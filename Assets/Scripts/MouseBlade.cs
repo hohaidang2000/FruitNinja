352ec9e2a9c3f55fd0ec03c7fee0878bb33111d2
Hồ Hải Đăng <72792170+hohaidang2000@users.noreply.github.com>
2022-04-07 16:27:53 +0700

    basic trail work

--- Assets/Scripts/MouseBlade.cs
+++ Assets/Scripts/MouseBlade.cs
@@ -6,12 +6,15 @@ public class MouseBlade : MonoBehaviour
 {
     GameInput playerInput;
     public Camera camera;
-    public Transform ball;
+    public Transform mouse;
+    public Transform trailDefault;
+    public Transform trailControl;
     bool isCutting;
-    
+    bool hasTrail;
     // Start is called before the first frame update
     private void Awake()
     {
+        hasTrail = false;
         isCutting = false;
         //Cursor.lockState = CursorLockMode.Locked;
         playerInput = new GameInput();
@@ -22,29 +25,47 @@ public class MouseBlade : MonoBehaviour
         };
         playerInput.Player.Slice.canceled += contex =>
         {
+            StopTrail();
             isCutting = false;
+            
         };
     }
-
+    void StopTrail()
+    {
+        trailControl.gameObject.SetActive(false);
+        Destroy(trailControl.gameObject, 2f);
+        hasTrail = false;
+    }
     void StartCutting()
     {
-        Vector2 pos = playerInput.Player.MousePosition.ReadValue<Vector2>();
-        Vector3 mousePosition = new Vector3(pos.x, pos.y, 10f);
-        mousePosition = camera.ScreenToWorldPoint(mousePosition);
+        
        
+        Vector2 pos = playerInput.Player.MousePosition.ReadValue<Vector2>();
+        Vector3 mousePosition = new Vector3(pos.x, pos.y, Mathf.Abs(camera.transform.position.z));
+        mousePosition = camera.ScreenToWorldPoint(mousePosition);
+
        
         mousePosition.z = 0;
-        ball.transform.position = mousePosition;
+        trailControl.transform.position = mousePosition;
+        //mouse.transform.position = mousePosition;
         Debug.Log(playerInput.Player.MousePosition.ReadValue<Vector2>());
         Debug.Log(mousePosition);
     }
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(playerInput.Player.MousePosition.ReadValue<Vector2>());
         if (isCutting)
         {
+            if (hasTrail == false)
+            {   
+                trailControl = Instantiate(trailDefault);
+                //trailControl.SetParent(mouse);
+                trailControl.gameObject.SetActive(true);
+                hasTrail = true;
+            }
             StartCutting();
         }
+        //Debug.Log(playerInput.Player.MousePosition.ReadValue<Vector2>());
+        
     }
 }
